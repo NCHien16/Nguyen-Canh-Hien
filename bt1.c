fddf27b3175b833377a2bef61650c8799ae73a72
NCHien16 <131459655+NCHien16@users.noreply.github.com>
2023-04-24 10:11:34 +0700

    Add files via upload

--- bt1.c
+++ bt1.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void arrrandom(int* mang[] , int a )
+void arrrandom(int mang[] , int a )
 {
     srand((int) time(0));
    for(int i = 0 ; i < a ; i++)
    {
-       mang[i] = rand()%10;
+       mang[i] = 10;
    }
 }
 
-void sapxep( int* mang[],int a)
+void sapxep( int mang[],int a )
 {
         for(int i = 0 ; i < a ; i++)
     {
@@ -26,19 +26,32 @@ void sapxep( int* mang[],int a)
         }   
     }
 }
-void lietke(int* mang[], int a)
+void lietke(int mang[], int a)
 {
-   
+    int dem[1000] = {0};
+    for(int i = 0 ; i < a ; i++)
+    {
+        dem[mang[i]]++;
+    }
+    for(int i = 0; i < a ; i++)
+    {
+        if(dem[mang[i]] !=0)
+        {
+            printf("\nSo %d xuat hien %d lan ", mang[i], dem[mang[i]]);
+            dem[mang[i]]=0;
+        }
+    }
 }
 
 
 int main()
 {
     int a;
+    int dem[100]={0};
     printf(" Nhap so phan tu cua mang: ");
     scanf("%d", &a);
     int mang[100];
-    arrrandom(mang , a);
+    arrrandom(mang ,a);
     printf(" \n Ta co mang tao ngau nhien");
     for(int i=0;i<a ;i++)
 		{
@@ -50,18 +63,6 @@ int main()
 		{
 			printf("%d ",mang[i]);
 		}
-     int dem[100]={0};
-    for(int i = 0 ; i < a ; i++)
-    {
-        dem[mang[i]]++;
-    }
-    for(int i = 0; i < a ; i++)
-    {
-        if(dem[mang[i]] !=0)
-        {
-            printf("\nso %d xuat hien %d lan ", mang[i], dem[mang[i]]);
-            dem[mang[i]]=0;
-        }
-    }
+   lietke(mang, a);
   return 0;
 }
